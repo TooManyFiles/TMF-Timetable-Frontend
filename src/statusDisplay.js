322b6b42ae9b47a474d7f6429c6c6554ad21530d
CodeModeYT <tillmann.menzer@outlook.com>
2024-09-23 20:22:25 +0200

    Added status display

---
+++ src/statusDisplay.js
@@ -0,0 +1,20 @@
+function setLastRefreshed(time){
+    document.getElementById('status-display-text').textContent = `Zuletzt aktualisiert: ${time}`;
+    document.getElementById('loader').style.display = 'none';
+}
+
+function setLoading(){
+    document.getElementById('status-display-text').textContent = 'Wird aktualisiert...';
+    document.getElementById('loader').style.display = 'flex';
+}
+
+function getCurrentTime(){
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth() + 1;
+    const day = now.getDate();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    return(`${day}.${month}.${year} um ${hours}:${minutes}:${seconds}`);
+}
