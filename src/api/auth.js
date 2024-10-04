6e3b5b5fe99a87cd883a90b99ff67e4877b5458a
Mr_Comand <74143930+Mr-Comand@users.noreply.github.com>
2024-10-04 05:35:33 +0200

    implemented /login.html

---
+++ src/api/auth.js
@@ -0,0 +1,44 @@
+import { apiurl } from "../config.js";
+
+// Login function
+export async function login(username, password) {
+    try {
+        const response = await fetch(apiurl + 'login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify({ username, password }),
+        });
+        const data = await response.json();
+        if (response.ok) {
+            localStorage.setItem('token', data.token);
+            return data;
+        } else {
+            throw new Error(data.message);
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+// Logout function
+export async function logout() {
+    try {
+        const token = localStorage.getItem('token');
+        if (token) {
+            const response = await fetch(apiurl + 'logout', {
+                method: 'POST',
+                credentials: "include"
+            });
+            if (response.ok) {
+                localStorage.removeItem('token');
+            } else {
+                throw new Error('Failed to logout');
+            }
+        }
+    } catch (error) {
+        throw error;
+    }
+}
