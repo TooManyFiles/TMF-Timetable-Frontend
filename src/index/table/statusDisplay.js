0a0acd4300cf21b5bec11056cdebc750c5557914
CodeModeYT <tillmann.menzer@outlook.com>
2024-10-17 15:16:35 +0200

    Fixes #40

--- src/index/table/statusDisplay.js
+++ src/index/table/statusDisplay.js
@@ -8,16 +8,17 @@ export function setLoading(){
     document.getElementById('loader').style.display = 'flex';
 }
 
-export function getCurrentTime(){
+export function getCurrentTime() {
     const now = new Date();
     const year = now.getFullYear();
     const month = now.getMonth() + 1;
     const day = now.getDate();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
     return(`${hours}:${minutes}:${seconds}`);
 }
+
 
 window.setLastRefreshed = setLastRefreshed;
 window.setLoading = setLoading;
