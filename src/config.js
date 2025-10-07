ca3a1d6d1829df0843b20fff8e3f02b0ca114e4c
Mr_Comand <74143930+Mr-Comand@users.noreply.github.com>
2025-10-07 16:02:40 +0200

    undo wrong commit

--- src/config.js
+++ src/config.js
@@ -1,9 +1,7 @@
-let API_URL_input = 'https://timetable.api.beta.toomanyfiles.dev/';
-// let API_URL_input = 'http://localhost:8080/';
-const hashPasswordBool_input = 'true';
+let API_URL_input = '${API_URL}';
+const hashPasswordBool_input = '${HASH_PASSWORD}';
 
 export const hashPasswordBool = /^(true|1|yes)$/i.test(hashPasswordBool_input.trim());
-
 
 if (!API_URL_input.endsWith('/')) {
     API_URL_input = API_URL_input + "/";
